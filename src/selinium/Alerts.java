74485d191bbf0401a96f76d51a405a67886ebf62
mounikakayyam <kayyammounika0212@gmail.com>
2024-02-23 16:19:34 +0530

    concept

--- src/selinium/Alerts.java
+++ src/selinium/Alerts.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Alerts {
-
+//concept
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver=new ChromeDriver();
 		
